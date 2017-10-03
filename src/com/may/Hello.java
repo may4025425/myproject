dca90b554d3b9b59982b817b6570d4a425123e50
may4025425 <may4025425@gmail.com>
2017-10-03 16:32:27 +0800

    first commit

---
+++ src/com/may/Hello.java
@@ -0,0 +1,13 @@
+package com.may;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+	
+      System.out.println("Hello World");
+      //
+      Person p = new Person();
+      p.sayHello();
+     }
+
+}
---
+++ src/com/may/Person.java
@@ -0,0 +1,16 @@
+package com.may;
+
+public class Person {
+	public void sayHello(){
+		System.out.println("Hello World");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person p = new Person();
+		p.sayHello();
+		}
+
+	}
+
+
