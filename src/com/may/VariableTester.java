3d3bab55c54d412d9624174930606f5a562cb167
Hank Tom <may4025425@gmail.com>
2017-10-17 16:31:40 +0800

    variable, primitive

---
+++ src/com/may/VariableTester.java
@@ -0,0 +1,24 @@
+package com.may;
+
+import java.util.Random;
+
+public class VariableTester {
+
+	public static void main(String[] args) {
+        int age = 0;
+        float weight = 69.5f;
+        char c = 65;
+        char c2 = 'Z';
+        boolean pass = true;
+        System.out.println(!pass);
+        System.out.println(pass);
+      
+        String s = new String("Hello");
+        Random r = new Random();
+        System.out.println(r.nextInt(6)+1);
+        System.out.println(3 >= 2);
+        		
+        
+	}
+
+}
