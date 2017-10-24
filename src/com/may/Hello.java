0edf6537da964b78d0a4ff8f96094c7b6e2bcd71
may4025425 <may4025425@gmail.cpm>
2017-10-24 16:41:15 +0800

    算術運算子

--- src/com/may/Hello.java
+++ src/com/may/Hello.java
@@ -1,5 +1,7 @@
 package com.may;
 
+import java.util.Scanner;
+
 public class Hello {
 	
 	public static void main(String[] args) {
@@ -8,6 +10,17 @@ public class Hello {
       //
       Person p = new Person();
       p.sayHello();
+      
+      System.out.println("請輸入您的名字");
+      Scanner scanner = new Scanner(System.in);
+      String line = scanner.nextLine();
+      System.out.println("Hello, " +line);
+      System.out.println("您幾歲?");
+      line = scanner.nextLine();
+      int age = Integer.parseInt(line);
+      System.out.println(age);
+      System.out.println(age > 25);
+      
      }
 
 }
--- src/com/may/VariableTester.java
+++ src/com/may/VariableTester.java
@@ -13,12 +13,29 @@ public class VariableTester {
         System.out.println(!pass);
         System.out.println(pass);
       
-        String s = new String("Hello");
+        
         Random r = new Random();
         System.out.println(r.nextInt(6)+1);
         System.out.println(3 >= 2);
-        		
+        String s = new String("Hello");
+        String s2 = "HAHA";
+        int len = s.length();
+        System.out.println("len:"+len);
+        System.out.println(s.charAt(0));
         
+        float f = 5.0f;
+        int x = 5;
+        int y = 3;
+        int z = (int)(f%y);//轉型(int)
+        System.out.println(z);
+        System.out.println(x >> 1);
+        x = x+1;
+        x++;
+        x--;
+        System.out.println(x);
+        x = x+2;
+        x += 2;
+        System.out.println(x);
 	}
 
 }
